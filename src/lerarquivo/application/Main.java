8ee6ca23497c628830ee20c3505a306b8cd1a9b4
Felipe Oliveira <fbmithrandir@outlook.com>
2023-07-24 20:46:47 -0300

    ler arquivo

---
+++ src/lerarquivo/application/Main.java
@@ -0,0 +1,30 @@
+package lerarquivo.application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		File file = new File("C:\\temp\\teste.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Error openning file: " + e.getLocalizedMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
